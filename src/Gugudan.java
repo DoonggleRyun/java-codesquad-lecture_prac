a2d45082302c3ade67452d0e679fca9b9821820e
seryun <devseryun@gmail.com>
2021-11-18 17:45:33 +0900

    gugu_scanner

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
@@ -6,6 +7,15 @@ public class Gugudan {
 		
 		for (int i = 1; i < 10; i++) {
 			System.out.println(2 * i);	
+		}
+		
+		
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int start =1, end = 9;
+		for (int i = start; i <= end; i++) {
+			System.out.println(number * i);	
 		}
 		
 	}
